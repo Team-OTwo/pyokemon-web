655ffd36e6bdd3238cd343d4baf73454567d674b
sky121016 <sky121016@naver.com>
2025-07-24 09:51:12 +0900

    [Feature/PYOKEMON-15] date-utils 삭제

--- src/utils/date-utils/date-utils.ts
+++
@@ -1,7 +0,0 @@
-export function formatDate(date: Date, format: string): string {
-  const year = date.getFullYear().toString()
-  const month = (date.getMonth() + 1).toString().padStart(2, "0")
-  const day = date.getDate().toString().padStart(2, "0")
-
-  return format.replace("YYYY", year).replace("MM", month).replace("DD", day)
-}
