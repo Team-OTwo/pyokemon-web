8769322f274228fd338e7f645c5279e70b68bfa9
sky121016 <sky121016@naver.com>
2025-07-29 12:56:18 +0900

    [Feature/#3/PYOKEMON-31] 상세 조회 페이지에 캘린더 추가

--- src/pages/event-detail/_component/booking-sidebar.tsx
+++ src/pages/event-detail/_component/booking-sidebar.tsx
@@ -1,33 +1,50 @@
-import React from "react"
-import { event } from "@/constants/event"
+import React, { useState } from "react"
 import { differenceInCalendarDays, format, parseISO } from "date-fns"
 import { ko } from "date-fns/locale"
+import Calendar from "react-calendar"
 
 import Button from "@/components/ui/button"
 
+import "react-calendar/dist/Calendar.css"
+import "./calendar.css"
+
+import { useEventStore } from "@/store/event/event-store"
+
 const BookingSidebar = () => {
+  const { event } = useEventStore()
+
+  if (!event) {
+    return <div>Loading...</div> // 로딩 중 UI
+  }
+
   const today = new Date()
-  const ticketOpenAt = parseISO(event.ticket_open_at)
+  const ticketOpenAt = parseISO(event.ticketOpenAt)
   const diffDays = differenceInCalendarDays(ticketOpenAt, today)
-  const isOpen = diffDays == 0
+  const isOpen = diffDays < 0
 
   return (
     <article className="w-480">
       <div className="w-320 fixed right-80 rounded-lg">
-        {/* 오픈 일정 */}
-        <div className="border-1 border-gray-300 rounded-lg text-center bg-white p-24 w-320 mb-32 shadow-[0px_0px_10px_0px_rgba(0,0,0,0.10)]">
-          <div className="flex items-center mb-12 gap-12">
-            <h3 className="font-bold text-lg">오픈 일정</h3>
-            <span className="text-primary font-semibold py-4 px-12 rounded-lg bg-primary/20 text-sm">
-              D-{diffDays}
-            </span>
+        {isOpen ? (
+          <div>
+            <Calendar value={event.eventDate} onClickDay={() => {}} locale="en-US" />
+            <Button text="예매하기" />
           </div>
-          <h1 className="text-primary font-semibold text-lg py-24 border-1 border-primary rounded-lg">
-            {format(event.ticket_open_at, "yyyy.MM.dd (iii) hh:mm", { locale: ko })}
-          </h1>
-        </div>
+        ) : (
+          // 오픈 일정
 
-        {isOpen && <Button text="예매하기" />}
+          <div className="border-1 border-gray-300 rounded-lg text-center bg-white p-24 w-320 mb-32 shadow-[0px_0px_10px_0px_rgba(0,0,0,0.10)]">
+            <div className="flex items-center mb-12 gap-12">
+              <h3 className="font-bold text-lg">오픈 일정</h3>
+              <span className="text-primary font-semibold py-4 px-12 rounded-lg bg-primary/20 text-sm">
+                D-{diffDays}
+              </span>
+            </div>
+            <h1 className="text-primary font-semibold text-lg py-24 border-1 border-primary rounded-lg">
+              {format(event.ticketOpenAt, "yyyy.MM.dd (iii) hh:mm", { locale: ko })}
+            </h1>
+          </div>
+        )}
       </div>
     </article>
   )
