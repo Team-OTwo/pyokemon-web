413fa0b417d8acee6f8735869545fdd684ad286c
sky121016 <sky121016@naver.com>
2025-08-02 14:48:49 +0900

    [PYOKEMON-57] [Feature/#18] 버튼 스타일 수정. small props 추가

--- src/components/ui/button/button.tsx
+++ src/components/ui/button/button.tsx
@@ -3,15 +3,18 @@ import React from "react"
 interface ButtonProps {
   text: string
   border?: boolean
+  small?: boolean
   onClick?: () => void
 }
-const Button: React.FC<ButtonProps> = ({ text, border, onClick }) => {
-  const baseStyle = "rounded-lg w-320 h-50 flex justify-center items-center cursor-pointer "
+const Button: React.FC<ButtonProps> = ({ text, border, small, onClick }) => {
+  const baseStyle =
+    "rounded-lg h-51 flex justify-center items-center cursor-pointer hover:opacity-80 "
+  const sizeStyle = small ? "px-24 " : "w-320 "
   const style = border
     ? "bg-white border-1 border-primary text-primary"
     : "bg-primary border-1 border-primary text-white"
   return (
-    <button className={baseStyle + style} onClick={onClick}>
+    <button className={baseStyle + sizeStyle + style} onClick={onClick}>
       {text}
     </button>
   )
