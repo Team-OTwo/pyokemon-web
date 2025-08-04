f2176d8fb40b5db523bc4281da92dfe23c04cb67
sky121016 <sky121016@naver.com>
2025-08-04 14:16:56 +0900

    [PYOKEMON-68] [Feature/#21] 장르별 공연 조회 페이지 구현

--- src/pages/event-list-page.tsx
+++ src/pages/event-list-page.tsx
@@ -1,29 +1,40 @@
-import React from "react"
+import React, { useState } from "react"
 import { useGetEventListQuery } from "@/api/event/queries/get-event-list-query"
+import { GENRE_LIST, GENRE_MAP } from "@/constants/genre"
 import { useSearchParams } from "react-router"
 
+import GenreBadge from "@/components/ui/genre-badge"
 import Pagination from "@/components/ui/pagination"
 import EventPreviewCard from "@/components/event-preview-card/event-preview-card"
 
 const EventListPage = () => {
   const [searchParams, setSearchParams] = useSearchParams()
-
-  const typeMap: Record<string, string> = {
-    concert: "콘서트",
-    musical: "뮤지컬",
-    classic: "클래식",
-    exhibition: "행사",
-    play: "연극",
-    sports: "스포츠",
-  }
-
-  const type = searchParams.get("type") ?? "concert"
+  const type = searchParams.get("type") ?? ""
   const page = Number(searchParams.get("page") ?? "1")
-  const genre = typeMap[type ?? ""] ?? "콘서트"
+  const genre = GENRE_MAP[type] ?? ""
+
   const { data: eventList, isLoading, error } = useGetEventListQuery(genre, page)
 
-  const goToPage = (newPage: number) => {
+  const handlePageChange = (newPage: number) => {
     setSearchParams({ type, page: newPage.toString() })
+  }
+
+  const handleGenreClick = (newType: string) => {
+    const currentType = searchParams.get("type") ?? ""
+    const nextParams = new URLSearchParams(searchParams)
+
+    if (newType === "") {
+      nextParams.delete("type")
+    } else if (newType === currentType) {
+      nextParams.delete("type")
+    } else {
+      nextParams.set("type", newType)
+    }
+
+    // 장르 바뀌면 페이지도 초기화
+    nextParams.delete("page")
+
+    setSearchParams(nextParams)
   }
 
   if (isLoading || !eventList) return <div>Loading...</div>
@@ -31,7 +42,22 @@ const EventListPage = () => {
 
   return (
     <div className="px-160 mb-48">
-      <h1 className="head1 py-32 text-center">{genre} 둘러보기</h1>
+      <h1 className="title-24-bold pt-48">공연 둘러보기</h1>
+
+      {/* filtering */}
+
+      <ul className="flex gap-12 py-24">
+        {GENRE_LIST.map((genre) => {
+          const isActive = type === genre.type
+          return (
+            <li key={genre.title} onClick={() => handleGenreClick(genre.type)}>
+              <GenreBadge genre={genre.title} gray={!isActive} />
+            </li>
+          )
+        })}
+      </ul>
+
+      {/* event */}
       <div className="grid grid-cols-3 gap-24">
         {eventList.map((event, i) => {
           return <EventPreviewCard key={i} event={event} />
@@ -43,7 +69,7 @@ const EventListPage = () => {
           current={page}
           total={eventList[0]?.total ?? 0}
           pageSize={8}
-          onChange={(p) => goToPage(p)}
+          onChange={(p) => handlePageChange(p)}
         />
       </div>
     </div>
