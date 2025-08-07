2a83920a7d00ef270fa88bb725eb7b78c0534e6b
sky121016 <sky121016@naver.com>
2025-08-07 17:38:05 +0900

    [PYOKEMON-112] [Feature/#32] 티켓 샘플데이터

---
+++ src/constants/ticket.ts
@@ -0,0 +1,54 @@
+export const tickets = [
+  {
+    bookingId: 1001,
+    userName: "가나다",
+    eventTitle: "오아시스 내한",
+    eventDate: "2025-08-06",
+    venueName: "서울 올림픽 체조경기장",
+    seat: {
+      class: "VIP",
+      floor: 1,
+      row: "D열",
+      col: "9",
+    },
+    thumbnailUrl:
+      "http://ticketimage.interpark.com/TCMS3.0//MProd/ProdBridge/2506/c9f64fa0-a6f2-428c-9f69-8fa8dd7acc02.png",
+    totalPrice: 151000,
+    status: "결제완료",
+  },
+  {
+    bookingId: 1002,
+    userName: "가나다",
+    eventTitle: "오아시스 내한",
+    eventDate: "2025-08-06",
+    venueName: "서울 올림픽 체조경기장",
+    seat: {
+      class: "VIP",
+      floor: 1,
+      row: "D열",
+      col: "9",
+    },
+    thumbnailUrl:
+      "http://ticketimage.interpark.com/TCMS3.0//MProd/ProdBridge/2506/c9f64fa0-a6f2-428c-9f69-8fa8dd7acc02.png",
+    totalPrice: 151000,
+    status: "결제완료",
+  },
+]
+
+export const ticket = {
+  bookingId: 1001,
+  userName: "가나다",
+  eventTitle: "오아시스 내한",
+  eventDate: "2025-08-06",
+  venueName: "서울 올림픽 체조경기장",
+  seat: {
+    class: "VIP",
+    floor: 1,
+    row: "D열",
+    col: "9",
+  },
+  thumbnailUrl:
+    "http://ticketimage.interpark.com/TCMS3.0//MProd/ProdBridge/2506/c9f64fa0-a6f2-428c-9f69-8fa8dd7acc02.png",
+  totalPrice: 151000,
+  status: "결제완료",
+}
