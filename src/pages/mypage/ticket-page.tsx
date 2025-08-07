4e0f8e2933e10b30d36efb92c84c459e2ec49ea1
sky121016 <sky121016@naver.com>
2025-08-07 17:39:15 +0900

    [PYOKEMON-112] [Feature/#32] ticket page에서 ticket card 렌더링

--- src/pages/mypage/ticket-page.tsx
+++ src/pages/mypage/ticket-page.tsx
@@ -1,7 +1,23 @@
 import React from "react"
+import { tickets } from "@/constants/ticket"
+
+import TicketCard from "@/components/ticket-card/ticket-card"
 
 const TicketPage = () => {
-  return <div>TicketPage</div>
+  return (
+    <div className="px-160 mb-48">
+      <h1 className="title-24-bold pt-48">내 예매 내역</h1>
+      <div className="flex flex-col gap-24">
+        {tickets.map((ticket) => {
+          return (
+            <li key={ticket.bookingId}>
+              <TicketCard ticket={ticket} />
+            </li>
+          )
+        })}
+      </div>
+    </div>
+  )
 }
 
 export default TicketPage
