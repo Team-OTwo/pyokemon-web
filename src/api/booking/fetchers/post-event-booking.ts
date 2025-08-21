d4393f678e6641ef5d17f3178aedc756020a1589
seoyoun8694 <chaeseoyoun8694@gmail.com>
2025-08-21 17:44:09 +0900

    [PYOKEMON-143] [feature] 좌석 예매시 redis hold 상태 #56

--- src/api/booking/fetchers/post-event-booking.ts
+++ src/api/booking/fetchers/post-event-booking.ts
@@ -1,5 +1,5 @@
 import { PostEventBookingRequest, PostEventBookingResponse } from "../../../types/booking"
-import { bookingClient, setAuthorizationHeader } from "../../client"
+import { bookingClient, eventClient, setAuthorizationHeader } from "../../client"
 
 export const postEventBooking = async (
   requestData: PostEventBookingRequest
@@ -13,6 +13,25 @@ export const postEventBooking = async (
   try {
     setAuthorizationHeader(accessToken)
     const res = await bookingClient.post("/api/bookings/booking", requestData)
+    console.log(res.data)
+
+    return res.data
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+export const postReidsBookiing = async (eventScheduleId: number, seatId: number) => {
+  const accessToken = localStorage.getItem("accessToken")
+
+  if (!accessToken) {
+    throw new Error("Access token not found")
+  }
+
+  try {
+    setAuthorizationHeader(accessToken)
+    const res = await eventClient.post(`/api/seats/${eventScheduleId}/${seatId}/hold`)
     console.log(res.data)
 
     return res.data
