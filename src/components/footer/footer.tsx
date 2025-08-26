0941cf778112f78e35c194d640b260f42ebcfc66
sky121016 <sky121016@naver.com>
2025-08-26 14:22:44 +0900

    [PYOKEMON-181] [Feature/#61] 푸터 제작

--- src/components/footer/footer.tsx
+++ src/components/footer/footer.tsx
@@ -1,5 +1,24 @@
+import { IoLogoInstagram, IoLogoYoutube, IoMailOutline } from "react-icons/io5"
+
 function Footer() {
-  return <footer className="bg-black h-100"></footer>
+  return (
+    <footer className="bg-black text-gray-300 h-100 flex justify-between items-center px-160">
+      <p className="text-center font-light text-sm">© 2025 PYOKEMON</p>
+
+      <div className="flex gap-12 text-black">
+        <div className="bg-white rounded-full p-8 w-32">
+          <IoMailOutline />
+        </div>
+        <div className="bg-white rounded-full p-8 w-32">
+          <IoLogoInstagram />
+        </div>
+
+        <div className="bg-white rounded-full p-8 w-32">
+          <IoLogoYoutube />
+        </div>
+      </div>
+    </footer>
+  )
 }
 
 export default Footer
