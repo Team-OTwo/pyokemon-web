84cb83737e035e0e691e685ce47d0db87c510d29
sky121016 <sky121016@naver.com>
2025-08-26 11:23:16 +0900

    [PYOKEMON-181] [Feature/#61] 조회 내역 없음 처리

--- src/pages/mypage/my-bookings-page.tsx
+++ src/pages/mypage/my-bookings-page.tsx
@@ -1,5 +1,6 @@
 import React from "react"
 import { useGetMyBookingsQuery } from "@/api/mypage/queries/use-get-my-bookings-query"
+import { IoTicketOutline } from "react-icons/io5"
 import { useNavigate, useSearchParams } from "react-router"
 
 import { Ticket } from "@/types/ticket"
@@ -29,54 +30,57 @@ const MyBookingsPage = () => {
     return <LoadingPage />
   }
 
-  if (!data || data === "") {
-    return (
-      <div className="px-160 mb-48">
-        <h1 className="title-24-bold pt-48">내 예매 내역</h1>
-        <p className="text-center py-120 text-gray-500">예매 내역이 없습니다.</p>
-        <div className="w-full flex justify-center items-center">
-          <Button
-            text="공연 예매하러 가기"
-            border
-            borderColor="primary"
-            bgColor="white"
-            color="primary"
-            onClick={() => navigate("/event")}
-          />
-        </div>
-      </div>
-    )
-  }
-
   if (error) {
     return <ErrorPage />
   }
 
   return (
     <div className="px-160 mb-48">
-      <h1 className="title-24-bold pt-48">내 예매 내역</h1>
-      <div className="flex flex-col gap-24 py-32">
-        {data.content.map((booking: Ticket) => {
-          return (
-            <li
-              key={booking.bookingId}
-              onClick={() => handleClick(booking.bookingId)}
-              className="cursor-pointer"
-            >
-              <TicketCard ticket={booking} />
-            </li>
-          )
-        })}
-      </div>
+      <h1 className="title-24-bold pt-48 flex items-center gap-12">
+        <IoTicketOutline className="text-black" />
+        예매 내역
+      </h1>
 
-      <div className="flex justify-center">
-        <Pagination
-          current={data.page + 1}
-          total={data.totalCount}
-          pageSize={10}
-          onChange={(p) => handlePageChange(p)}
-        />
-      </div>
+      {!data || data === "" ? (
+        <>
+          <p className="text-center py-120 text-gray-500">예매 내역이 없습니다.</p>
+          <div className="w-full flex justify-center items-center">
+            <Button
+              text="공연 예매하러 가기"
+              border
+              borderColor="primary"
+              bgColor="white"
+              color="primary"
+              onClick={() => navigate("/event")}
+            />
+          </div>
+        </>
+      ) : (
+        <>
+          <div className="flex flex-col gap-24 py-32">
+            {data.content.map((booking: Ticket) => {
+              return (
+                <li
+                  key={booking.bookingId}
+                  onClick={() => handleClick(booking.bookingId)}
+                  className="cursor-pointer"
+                >
+                  <TicketCard ticket={booking} />
+                </li>
+              )
+            })}
+          </div>
+
+          <div className="flex justify-center">
+            <Pagination
+              current={data.page + 1}
+              total={data.totalCount}
+              pageSize={10}
+              onChange={(p) => handlePageChange(p)}
+            />
+          </div>
+        </>
+      )}
     </div>
   )
 }
