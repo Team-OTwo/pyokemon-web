cfa39e738073f19c2a8c579a8bb17f419eff6106
sky121016 <sky121016@naver.com>
2025-08-27 11:03:11 +0900

    [PYOKEMON-192] [Feature/#63] 알림 ui 수정

--- src/components/notification/notification-item.tsx
+++ src/components/notification/notification-item.tsx
@@ -1,6 +1,7 @@
-import React from "react"
+import React, { useState } from "react"
 import { format } from "date-fns"
 import { ko } from "date-fns/locale"
+import { IoChevronDown, IoChevronUp } from "react-icons/io5"
 
 interface NotificationItemProps {
   title: string
@@ -15,16 +16,27 @@ const NotificationItem: React.FC<NotificationItemProps> = ({
   isRead,
   createdAt,
 }) => {
+  const bgColor = isRead ? "bg-white" : "bg-primary/10"
+
+  const [open, setOpen] = useState(false)
+
   return (
-    <div className="p-16 border-b-1 border-gray-100 flex justify-between items-center bg-primary/10">
-      <div>
-        <p className="text-16-bold">{title}</p>
-        <p className="text-16-medium mb-8">{message}</p>
-        <p className="text-14-medium text-gray-700">
-          {format(createdAt, "yyyy.MM.dd hh:mm", { locale: ko })}
-        </p>
+    <div
+      className={`p-16 border-b-1 border-gray-100 flex justify-between items-center cursor-pointer ${bgColor}`}
+      onClick={() => setOpen(!open)}
+    >
+      <div className="w-full">
+        <div className="w-full flex items-center justify-between mb-4">
+          <div>
+            <p className="text-16-bold">{title}</p>
+            <p className="text-14-normal text-gray-500">
+              {format(createdAt, "yyyy.MM.dd hh:mm", { locale: ko })}
+            </p>
+          </div>
+          <div className="text-gray-700 p-4">{open ? <IoChevronUp /> : <IoChevronDown />}</div>
+        </div>
+        {open && <p className="text-14-medium text-gray-700">{message}</p>}
       </div>
-      {!isRead && <div className="rounded-full w-10 h-10 bg-primary"></div>}
     </div>
   )
 }
